5d23295d0692367eb400186b0f77cc95c32fba75
haeramkeem <ewqdsacxz2345@gmail.com>
2024-06-08 02:48:49 +0900

    prototype: Iodine signature example

--- src/io.go
+++ src/io.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"bytes"
 	"encoding/hex"
 	"encoding/json"
+	"fmt"
 	"os"
 
 	"github.com/google/gopacket"
@@ -46,6 +48,15 @@ type OutputJson struct {
 func WriteFile(packet gopacket.Layer, fileName string) error {
 	output := new(OutputJson)
 	output.Packet = hex.EncodeToString(packet.LayerContents())
+	if !checkIodineSignature1(packet.LayerContents()) {
+		fmt.Fprintln(os.Stderr, "1", fileName, packet.LayerContents())
+	}
+	if !checkIodineSignature2(packet.LayerContents()) {
+		fmt.Fprintln(os.Stderr, "2", fileName, packet.LayerContents())
+	}
+	if !checkIodineSignature3(packet.LayerContents()) {
+		fmt.Fprintln(os.Stderr, "3", fileName, packet.LayerContents())
+	}
 	output.Key = KEY
 	output.Nonce = NONCE
 	output.Counter = PRE_COUNTER_SUFFIX
@@ -60,4 +71,51 @@ func WriteFile(packet gopacket.Layer, fileName string) error {
 	}
 
 	return nil
+}
+
+func checkIodineSignature1(mainSlice []byte) bool {
+	sigBytes := []byte{0x01, 0x00, 0x00, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x01}
+	sigOffset := 2
+	mainLen, sigLen := len(mainSlice), len(sigBytes)
+
+	if sigLen == 0 || sigLen > mainLen {
+		return false
+	}
+
+	if bytes.Compare(mainSlice[sigOffset:sigOffset+sigLen], sigBytes) == 0 {
+		return true
+	}
+	return false
+}
+
+func checkIodineSignature2(mainSlice []byte) bool {
+	sigBytes := []byte{0x45, 0x10}
+	sigOffset := 13
+	mainLen, sigLen := len(mainSlice), len(sigBytes)
+
+	if sigLen == 0 || sigLen > mainLen {
+		return false
+	}
+
+	if bytes.Compare(mainSlice[sigOffset:sigOffset+sigLen], sigBytes) == 0 {
+		return true
+	}
+	return false
+}
+
+func checkIodineSignature3(mainSlice []byte) bool {
+	sigBytes := []byte{0x00, 0x00, 0x29, 0x10, 0x00, 0x00, 0x00, 0x80, 0x00, 0x00, 0x00}
+	// sigOffset := nil // Arbirarily offset
+	mainLen, sigLen := len(mainSlice), len(sigBytes)
+
+	if sigLen == 0 || sigLen > mainLen {
+		return false
+	}
+
+	for i := 0; i <= mainLen-sigLen; i++ {
+		if bytes.Compare(mainSlice[i:i+sigLen], sigBytes) == 0 {
+			return true
+		}
+	}
+	return false
 }

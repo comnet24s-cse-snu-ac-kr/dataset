92fbf91543cea99833c14cafc328ea3368c82865
haeramkeem <ewqdsacxz2345@gmail.com>
2024-06-13 19:45:27 +0900

    feat: add snort rule checker

--- src/io.go
+++ src/io.go
@@ -48,15 +48,18 @@ type OutputJson struct {
 func WriteFile(packet gopacket.Layer, fileName string) error {
 	output := new(OutputJson)
 	output.Packet = hex.EncodeToString(packet.LayerContents())
-	if !checkIodineSignature1(packet.LayerContents()) {
-		fmt.Fprintln(os.Stderr, "1", fileName, packet.LayerContents())
-	}
-	if !checkIodineSignature2(packet.LayerContents()) {
-		fmt.Fprintln(os.Stderr, "2", fileName, packet.LayerContents())
-	}
-	if !checkIodineSignature3(packet.LayerContents()) {
-		fmt.Fprintln(os.Stderr, "3", fileName, packet.LayerContents())
-	}
+
+  sigs := make([]*SnortSignature, 0)
+  sigs = append(sigs, &SnortSignature{content: []byte{0x01, 0x00, 0x00, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x01}, offset: 2})
+  sigs = append(sigs, &SnortSignature{content: []byte{0x45, 0x10}, offset: 13})
+  sigs = append(sigs, &SnortSignature{content: []byte{0x00, 0x00, 0x29, 0x10, 0x00, 0x00, 0x00, 0x80, 0x00, 0x00, 0x00}, offset: -1})
+
+  for i, sig := range sigs {
+    if sig.check(packet.LayerContents()) {
+      fmt.Fprintln(os.Stderr, "Detected:", i, fileName, packet.LayerContents())
+    }
+  }
+
 	output.Key = KEY
 	output.Nonce = NONCE
 	output.Counter = PRE_COUNTER_SUFFIX
@@ -73,49 +76,29 @@ func WriteFile(packet gopacket.Layer, fileName string) error {
 	return nil
 }
 
-func checkIodineSignature1(mainSlice []byte) bool {
-	sigBytes := []byte{0x01, 0x00, 0x00, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x01}
-	sigOffset := 2
-	mainLen, sigLen := len(mainSlice), len(sigBytes)
-
-	if sigLen == 0 || sigLen > mainLen {
-		return false
-	}
-
-	if bytes.Compare(mainSlice[sigOffset:sigOffset+sigLen], sigBytes) == 0 {
-		return true
-	}
-	return false
+type SnortSignature struct {
+  content []byte
+  offset  int
 }
 
-func checkIodineSignature2(mainSlice []byte) bool {
-	sigBytes := []byte{0x45, 0x10}
-	sigOffset := 13
-	mainLen, sigLen := len(mainSlice), len(sigBytes)
+func (s *SnortSignature) check(mainSlice []byte) bool {
+	mainLen, sigLen := len(mainSlice), len(s.content)
 
 	if sigLen == 0 || sigLen > mainLen {
 		return false
 	}
 
-	if bytes.Compare(mainSlice[sigOffset:sigOffset+sigLen], sigBytes) == 0 {
-		return true
-	}
-	return false
-}
+  if s.offset == -1 {
+    for i := 0; i <= mainLen-sigLen; i++ {
+      if bytes.Compare(mainSlice[i:i+sigLen], s.content) == 0 {
+        return true
+      }
+    }
+  } else {
+    if bytes.Compare(mainSlice[s.offset:s.offset+sigLen], s.content) == 0 {
+      return true
+    }
+  }
 
-func checkIodineSignature3(mainSlice []byte) bool {
-	sigBytes := []byte{0x00, 0x00, 0x29, 0x10, 0x00, 0x00, 0x00, 0x80, 0x00, 0x00, 0x00}
-	// sigOffset := nil // Arbirarily offset
-	mainLen, sigLen := len(mainSlice), len(sigBytes)
-
-	if sigLen == 0 || sigLen > mainLen {
-		return false
-	}
-
-	for i := 0; i <= mainLen-sigLen; i++ {
-		if bytes.Compare(mainSlice[i:i+sigLen], sigBytes) == 0 {
-			return true
-		}
-	}
 	return false
 }

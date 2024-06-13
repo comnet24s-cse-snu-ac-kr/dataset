46d8c377564e501a89b57207df772cc539aed21f
haeramkeem <ewqdsacxz2345@gmail.com>
2024-06-13 20:44:44 +0900

    chore: remove constants

--- src/io.go
+++ src/io.go
@@ -1,10 +1,8 @@
 package main
 
 import (
-	"bytes"
 	"encoding/hex"
 	"encoding/json"
-	"fmt"
 	"os"
 
 	"github.com/google/gopacket"
@@ -36,33 +34,11 @@ func ReadFile(pcapFileName string) ([]gopacket.Layer, error) {
 
 type OutputJson struct {
 	Packet string `json:"packet"`
-	Key    string `json:"key"`
-	Nonce  string `json:"nonce"`
-
-	// Note that the word "couter" indicates suffix for PreCounterBlock (J0)
-	// which is 0x00000001 for 12-byte nonce (IV).
-	// See NIST SP 800-38D, section 7.1.
-	Counter string `json:"counter"`
 }
 
 func WriteFile(packet gopacket.Layer, fileName string) error {
 	output := new(OutputJson)
 	output.Packet = hex.EncodeToString(packet.LayerContents())
-
-  sigs := make([]*SnortSignature, 0)
-  sigs = append(sigs, &SnortSignature{content: []byte{0x01, 0x00, 0x00, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x01}, offset: 2})
-  sigs = append(sigs, &SnortSignature{content: []byte{0x45, 0x10}, offset: 13})
-  sigs = append(sigs, &SnortSignature{content: []byte{0x00, 0x00, 0x29, 0x10, 0x00, 0x00, 0x00, 0x80, 0x00, 0x00, 0x00}, offset: -1})
-
-  for i, sig := range sigs {
-    if sig.check(packet.LayerContents()) {
-      fmt.Fprintln(os.Stderr, "Detected:", i, fileName, packet.LayerContents())
-    }
-  }
-
-	output.Key = KEY
-	output.Nonce = NONCE
-	output.Counter = PRE_COUNTER_SUFFIX
 
 	dat, err := json.Marshal(output)
 	if err != nil {
@@ -74,31 +50,4 @@ func WriteFile(packet gopacket.Layer, fileName string) error {
 	}
 
 	return nil
-}
-
-type SnortSignature struct {
-  content []byte
-  offset  int
-}
-
-func (s *SnortSignature) check(mainSlice []byte) bool {
-	mainLen, sigLen := len(mainSlice), len(s.content)
-
-	if sigLen == 0 || sigLen > mainLen {
-		return false
-	}
-
-  if s.offset == -1 {
-    for i := 0; i <= mainLen-sigLen; i++ {
-      if bytes.Compare(mainSlice[i:i+sigLen], s.content) == 0 {
-        return true
-      }
-    }
-  } else {
-    if bytes.Compare(mainSlice[s.offset:s.offset+sigLen], s.content) == 0 {
-      return true
-    }
-  }
-
-	return false
 }
